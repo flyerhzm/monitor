f60a6bee9951ad4b9cdcf3ee50f1e0f035434291
Richard <richard@ekohe.com>
2009-10-19 14:25:19 +0800

    add executable call_stack

--- lib/call_stack.rb
+++ lib/call_stack.rb
@@ -2,9 +2,3 @@ require 'call_stack/controller'
 require 'call_stack/server'
 require 'call_stack/unroller'
 require 'call_stack/unroller_server'
-require 'thread'
-
-Thread.start do 
-  UnrollerServer.new.run 
-end
-CallStackApp.run! :host => 'localhost', :port => 9090
