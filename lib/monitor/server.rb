793bb07fdf1874037b81f3fa186d5f24a5439301
Richard <richard@ekohe.com>
2009-10-20 11:04:17 +0800

    use static for html, js, css and png

--- lib/monitor/server.rb
+++ lib/monitor/server.rb
@@ -2,35 +2,15 @@ require 'rubygems'
 require 'sinatra'
 
 class MonitorApp < Sinatra::Base
-  set :public, File.dirname(__FILE__) + '/static'
-  set :views, File.dirname(__FILE__) + '/templates'
+  set :static, true
+  set :public, Proc.new { File.expand_path(File.dirname(__FILE__) + '/../../public') }
+  puts Proc.new { File.expand_path(File.dirname(__FILE__) + '/../../public') }
   
-  require 'erb'
   
   get '/monitor/request' do
     while !Controller.instance.push?
       sleep 1
     end
     "<div>#{Controller.instance.outputs.shift}</div>"
-  end
-  
-  get '/javascripts/jquery-1.3.2.min.js' do
-    send_file File.dirname(__FILE__) + '/static/javascripts/jquery-1.3.2.min.js'
-  end
-
-  get '/javascripts/monitor.js' do
-    send_file File.dirname(__FILE__) + '/static/javascripts/monitor.js'
-  end
-  
-  get '/stylesheets/monitor.css' do
-    send_file File.dirname(__FILE__) + '/static/stylesheets/monitor.css'
-  end
-  
-  get '/images/icons.png' do
-    send_file File.dirname(__FILE__) + '/static/images//icons.png'
-  end
-  
-  get '/' do
-    erb :index
   end
 end
