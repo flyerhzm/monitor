13ffeb76edddbded84c677d0ee7e269f50dd4d31
Richard <richard@ekohe.com>
2009-10-20 14:31:11 +0800

    add options server

--- lib/monitor/command.rb
+++ lib/monitor/command.rb
@@ -1,12 +1,16 @@
 require 'thread'
 
-options = {:browser_port => 9090, :unroller_port => 9099}
+options = {:browser_port => 9090, :unroller_port => 9099, :server => 'thin'}
 
 OptionParser.new do |opts|
   opts.banner = "Usage: monitor [options]"
 
   opts.on('-p', '--port BROWSER_PORT', Integer, 'the port which browser use to monitor codes') do |p|
-    options[:browser_port] = p
+    options[:port] = p
+  end
+  
+  opts.on('-s', '--server SERVER_HANDLER', 'specify rack server/handler (default is thin)') do |s|
+    options[:server] = s
   end
   
   opts.on('-u', '--unroller_port UNROLLER_PORT', Integer, 'the unroller port which is used to connect from and to source codes') do |u|
@@ -27,4 +31,4 @@ end
 Thread.start do 
   UnrollerServer.new(options[:unroller_port]).run 
 end
-MonitorApp.run! :host => 'localhost', :port => options[:browser_port]
+MonitorApp.run! :host => 'localhost', :port => options[:port], :server => options[:server]
